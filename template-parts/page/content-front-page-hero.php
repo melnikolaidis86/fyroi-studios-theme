b677e2d47f04881e243c055e7743f9e6b7f8199b
Meletis <melnikolaidis86@gmail.com>
2018-11-07 02:24:01 +0200

    Completed Hero Section

--- template-parts/page/content-front-page-hero.php
+++ template-parts/page/content-front-page-hero.php
@@ -8,15 +8,15 @@
 
 ?>
 
-<section class="site-hero overlay" data-stellar-background-ratio="0.5" style="background-image: url('<?php echo get_template_directory_uri() ?>/assets/images/big_image_1.jpg');">
+<section class="site-hero overlay" data-stellar-background-ratio="0.5" style="background-image: url('<?php echo get_theme_mod("fs_hero_background_image", get_template_directory_uri() . "/assets/images/big_image_1.jpg")  ?>');">
     <div class="container">
         <div class="row align-items-center site-hero-inner justify-content-center">
             <div class="col-md-12 text-center">
 
                 <div class="mb-5 element-animate">
-                    <h1>Welcome To Our Luxury Rooms</h1>
-                    <p>Discover our world's #1 Luxury Room For VIP.</p>
-                    <p><a href="booknow.html" class="btn btn-primary">Book Now</a></p>
+                    <h1><?php echo get_theme_mod('fs_hero_title', 'Welcome To Our Luxury Rooms'); ?></h1>
+                    <p><?php echo get_theme_mod('fs_hero_subtitle', 'Discover our world\'s #1 Luxury Room For VIP.'); ?></p>
+                    <p><a href="booknow.html" class="btn btn-primary"><?php _e('Check Our Rooms', 'fyroi_theme') ?></a></p>
                 </div>
 
             </div>
